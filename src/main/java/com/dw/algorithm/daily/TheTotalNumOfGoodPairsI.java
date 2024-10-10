d39bfad8bca3f83ba5d754019702c66e2dc56002
deathwhispers <guangjian.yang@qq.com>
2024-10-10 10:17:04 +0800

    优质数对的总数 I

--- src/main/java/com/dw/algorithm/daily/TheTotalNumOfGoodPairsI.java
+++ src/main/java/com/dw/algorithm/daily/TheTotalNumOfGoodPairsI.java
@@ -22,4 +22,16 @@ public class TheTotalNumOfGoodPairsI {
         return result;
     }
 
+    public int numberOfPairs2(int[] nums1, int[] nums2, int k) {
+        int result = 0;
+        for (int n1 : nums1) {
+            for (int n2 : nums2) {
+                if (n1 % (n2 * k) == 0) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
 }
