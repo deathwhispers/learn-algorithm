468aedd1e21f1ea5f027f6b020606d867991d81c
杨光剑 <guangjian.yang@qq.com>
2024-12-27 10:42:10 +0800

    存在重复元素 II  滑动窗口

--- src/main/java/com/dw/algorithm/classicinterview/ContainsNearbyDuplicate.java
+++ src/main/java/com/dw/algorithm/classicinterview/ContainsNearbyDuplicate.java
@@ -1,5 +1,7 @@
 package com.dw.algorithm.classicinterview;
 
+import java.util.HashSet;
+
 /**
  * 存在重复元素 II
  *
@@ -10,6 +12,10 @@ package com.dw.algorithm.classicinterview;
  */
 public class ContainsNearbyDuplicate {
 
+
+    /**
+     * 双指针，双层遍历
+     */
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
@@ -17,6 +23,27 @@ public class ContainsNearbyDuplicate {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 滑动窗口
+     * 只需要判断滑动窗口中是否存在重复元素即可
+     * 通过Set的元素不重复属性来实现
+     */
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > k) {
+                // i > k 后，将最左侧元素移出滑动窗口
+                set.remove(nums[i - k - 1]);
+            }
+            // add时，如果false 则说明滑动窗口中有重复元素了
+            if (!set.add(nums[i])) {
+                return true;
             }
         }
         return false;
