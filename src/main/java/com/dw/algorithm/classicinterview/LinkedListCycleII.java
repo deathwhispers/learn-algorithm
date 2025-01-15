c871a91de66e1cd4f2b919b58a97d242c6be917e
deathwhispers <guangjian.yang@qq.com>
2025-01-15 14:41:42 +0800

    环形链表 II

--- src/main/java/com/dw/algorithm/classicinterview/LinkedListCycleII.java
+++ src/main/java/com/dw/algorithm/classicinterview/LinkedListCycleII.java
@@ -2,13 +2,15 @@ package com.dw.algorithm.classicinterview;
 
 import com.dw.common.ListNode;
 
-import java.util.HashMap;
 import java.util.HashSet;
 
 /**
+ * 环形链表 II
+ *
  * @author yanggj
  * @version 1.0.0
  * @date 2025/1/3 10:15
+ * @see <a href="https://leetcode.cn/problems/linked-list-cycle-ii/">环形链表 II</a>
  */
 public class LinkedListCycleII {
 
@@ -17,7 +19,7 @@ public class LinkedListCycleII {
         while (head != null) {
             if (set.contains(head)) {
                 return head;
-            }else {
+            } else {
                 set.add(head);
             }
             head = head.next;
