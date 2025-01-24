63793e5bdb19461589b9eef7b24c2a715b4d793e
deathwhispers <guangjian.yang@qq.com>
2025-01-24 14:50:06 +0800

    定长滑动窗口 得到 K 个黑块的最少涂色次数

--- src/main/java/com/dw/algorithm/slidingwindow/fixedlength/MinRecolorsToGetKConsecutiveBlackBlocks.java
+++ src/main/java/com/dw/algorithm/slidingwindow/fixedlength/MinRecolorsToGetKConsecutiveBlackBlocks.java
@@ -8,7 +8,7 @@ package com.dw.algorithm.slidingwindow.fixedlength;
  * @date 2025/1/24 14:34
  * <a href="https://leetcode.cn/problems/minimum-recolors-to-get-k-consecutive-black-blocks/description/">得到 K 个黑块的最少涂色次数</a>
  */
-public class MinimumRecolorsToGetKConsecutiveBlackBblocks {
+public class MinRecolorsToGetKConsecutiveBlackBlocks {
 
     public int minimumRecolors(String blocks, int k) {
         char[] arr = blocks.toCharArray();
