ddaec2a15824b324879c1e6d5bd63a28c818d57b
deathwhispers <guangjian.yang@qq.com>
2025-02-11 15:02:03 +0800

    定长滑动窗口 几乎唯一子数组的最大和

--- src/main/java/com/dw/algorithm/slidingwindow/fixedlength/MaxSumOfAlmostUniqueSubarray.java
+++ src/main/java/com/dw/algorithm/slidingwindow/fixedlength/MaxSumOfAlmostUniqueSubarray.java
@@ -31,16 +31,24 @@ public class MaxSumOfAlmostUniqueSubarray {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             // 右侧元素移入，同时在 map 中记录该元素出现的次数
-            sum += nums.get(i);
-            map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) + 1);
+            Integer right = nums.get(i);
+            sum += right;
+            map.put(right, map.getOrDefault(right, 0) + 1);
+
+            // 窗口长度不足 k 时，继续
             if (i < k - 1) {
                 continue;
             }
+
+            // 窗口中不同元素个数应超过 m 个，不足时，不计入结果
             if (map.size() >= m) {
                 ans = Math.max(ans, sum);
             }
+            // 左侧元素出窗口，元素和中减掉该元素
             Integer left = nums.get(i - k + 1);
             sum -= left;
+
+            // 更新 map，若 map 中只有一个该元素，则删除 key，否则个数 -1
             Integer count = map.get(left);
             if (count == 1) {
                 map.remove(left);
@@ -50,6 +58,5 @@ public class MaxSumOfAlmostUniqueSubarray {
         }
         return ans;
     }
-
 
 }
