9a49842f1425b9d94e9fdbdc9ac2808b7d9d1b9e
deathwhispers <guangjian.yang@qq.com>
2025-02-14 15:43:58 +0800

    定长滑动窗口 检查一个字符串是否包含所有长度为 K 的二进制子串

---
+++ src/main/java/com/dw/algorithm/slidingwindow/fixedlength/CheckStrHasAllBinCodes.java
@@ -0,0 +1,27 @@
+package com.dw.algorithm.slidingwindow.fixedlength;
+
+import java.util.HashSet;
+
+/**
+ * 检查一个字符串是否包含所有长度为 K 的二进制子串
+ *
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2025/2/14 14:58
+ * <a href ="https://leetcode.cn/problems/check-if-a-string-contains-all-binary-codes-of-size-k/description/>检查一个字符串是否包含所有长度为 K 的二进制子串</a>
+ */
+public class CheckStrHasAllBinCodes {
+    public static void main(String[] args) {
+        CheckStrHasAllBinCodes test = new CheckStrHasAllBinCodes();
+        boolean b = test.hasAllCodes("00110", 2);
+        System.out.println(b);
+    }
+
+    public boolean hasAllCodes(String s, int k) {
+        HashSet<String> hashSet = new HashSet<>();
+        for (int i = 0; i + k <= s.length(); i++) {
+            hashSet.add(s.substring(i, i + k));
+        }
+        return hashSet.size() >= Math.pow(2, k);
+    }
+}
