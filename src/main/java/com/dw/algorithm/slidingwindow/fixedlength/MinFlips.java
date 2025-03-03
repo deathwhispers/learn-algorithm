4ef3cc092e1f31b1ed425b82cd4f2cd7ccd3ad62
deathwhispers <guangjian.yang@qq.com>
2025-03-03 15:10:34 +0800

    不定长滑动窗口 使二进制字符串字符交替的最少反转次数

---
+++ src/main/java/com/dw/algorithm/slidingwindow/fixedlength/MinFlips.java
@@ -0,0 +1,24 @@
+package com.dw.algorithm.slidingwindow.fixedlength;
+
+/**
+ * 使二进制字符串字符交替的最少反转次数
+ * 给你一个二进制字符串 s 。你可以按任意顺序执行以下两种操作任意次：
+ * <p>
+ * 类型 1 ：删除 字符串 s 的第一个字符并将它 添加 到字符串结尾。
+ * 类型 2 ：选择 字符串 s 中任意一个字符并将该字符 反转 ，也就是如果值为 '0' ，则反转得到 '1' ，反之亦然。
+ * 请你返回使 s 变成 交替 字符串的前提下， 类型 2 的 最少 操作次数 。
+ * <p>
+ * 我们称一个字符串是 交替 的，需要满足任意相邻字符都不同。
+ * <p>
+ * 比方说，字符串 "010" 和 "1010" 都是交替的，但是字符串 "0100" 不是。
+ *
+ * @author yanggj
+ * @version 1.0.0
+ * @date 2025/2/21 17:27
+ * <a href = "https://leetcode.cn/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/">使二进制字符串字符交替的最少反转次数</a>
+ */
+public class MinFlips {
+
+
+
+}
